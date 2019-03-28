45959a96940035ec2603e6b68f5d2d4daa1e9e94
Bianca <jabeuforia91@gmail.com>
2019-03-27 21:42:13 -0600

    archivo sql de prueba

---
+++ bdemployees.sql
@@ -0,0 +1,5 @@
+#SELECT * FROM dbemployees.employees
+insert into employees
+values(4, '1982-12-20', 'Juan', 'Tinajero', 'M', '2008-02-01');
+insert into employees
+values(4, '1982-12-20', 'Jose Luis', 'De La Rosa', 'M', '2009-02-01');
